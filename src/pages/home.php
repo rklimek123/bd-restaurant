4abfad6fc5ed08760952f561abe1e53f73edbec3
TheBlueshock <rafal1klimek@gmail.com>
2021-01-29 03:43:23 +0100

    ultra basic homepage [WIP]

---
+++ src/pages/home.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <link rel="icon" href="../resources/icon.jpg"/>
+    <title>Logowanie - Burger Familia</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <div style="text-align: center;"><h2> Zaloguj się </h2></div>
+    
+
+  </body>
+</html>
