a5edeecc12229876837993cc036494c0128f89bb
TheBlueshock <rafal1klimek@gmail.com>
2021-02-01 13:59:13 +0100

    sign_out

---
+++ src/pages/sign_out.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <link rel="icon" href="../resources/hermes-rivera-gv_XRp4dUqM-unsplash.jpg"/>
+    <title>Wyloguj - Burger Familia</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php
+session_start();
+session_unset();
+?>
+<div style="text-align: center;">
+    <h1>Wylogowanie</h1>
+    <p><i>Wylogowano pomyślnie</i></p>
+    <p><a href="home.php">Powrót na stronę tytułową</a></p>
+</div>
+</body>
